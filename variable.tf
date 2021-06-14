9ef66ce2d3ae45b9ef0360ed23b5e142e77c6455
Rafael Gomes <linux.rafa@gmail.com>
2021-06-14 16:20:20 -0300

    Fix variable name

--- variable.tf
+++ variable.tf
@@ -30,7 +30,7 @@ variable "primary_nameserver" {
   default = "1.1.1.1"
   validation {
     # regex(...) fails if it cannot find a match
-    condition     = can(regex("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", var.gateway4))
+    condition     = can(regex("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", var.primary_nameserver))
     error_message = "The primary_nameserver value must be ip address without subnet mask (ex. 192.168.0.1 )."
   }
 }
@@ -40,7 +40,7 @@ variable "secondary_nameserver" {
   default = "8.8.8.8"
   validation {
     # regex(...) fails if it cannot find a match
-    condition     = can(regex("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", var.gateway4))
+    condition     = can(regex("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", var.secondary_nameserver))
     error_message = "The secondary_nameserver value must be ip address without subnet mask (ex. 192.168.0.1 )."
   }
 }
